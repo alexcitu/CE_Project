3a9e93990f2a663f462a34a98f5d5b4594c184a0
Bogdan C <tunetu_bog@yahoo.com>
2017-01-08 18:59:56 +0200

    [CE-5363719] Traducere site in engleza/romana

--- upload/admin/language/ro-ro/ro-ro.php
+++ upload/admin/language/ro-ro/ro-ro.php
@@ -1,31 +1,31 @@
 <?php
 // Locale
-$_['code']                          = 'en';
-$_['direction']                     = 'ltr';
-$_['date_format_short']             = 'd/m/Y';
-$_['date_format_long']              = 'l dS F Y';
-$_['time_format']                   = 'h:i:s A';
-$_['datetime_format']               = 'd/m/Y H:i:s';
-$_['decimal_point']                 = '.';
-$_['thousand_point']                = ',';
+$_['code']                    = 'ro';
+$_['direction']               = 'ltr';
+$_['date_format_short']       = 'd.m.Y';
+$_['date_format_long']        = 'l dS F Y';
+$_['time_format']             = 'h:i:s A';
+$_['datetime_format']         = 'd/m/Y H:i:s';
+$_['decimal_point']           = ',';
+$_['thousand_point']          = '.';
 
 // Text
-$_['text_yes']                      = 'Yes';
-$_['text_no']                       = 'No';
-$_['text_enabled']                  = 'Enabled';
-$_['text_disabled']                 = 'Disabled';
-$_['text_none']                     = ' --- None --- ';
-$_['text_select']                   = ' --- Please Select --- ';
-$_['text_select_all']               = 'Select All';
-$_['text_unselect_all']             = 'Unselect All';
-$_['text_all_zones']                = 'All Zones';
-$_['text_default']                  = ' <b>(Default)</b>';
-$_['text_close']                    = 'Close';
-$_['text_pagination']               = 'Showing %d to %d of %d (%d Pages)';
-$_['text_loading']                  = 'Loading...';
-$_['text_no_results']               = 'No results!';
-$_['text_confirm']                  = 'Are you sure?';
-$_['text_home']                     = 'Home';
+$_['text_yes']                = 'Da';
+$_['text_no']                 = 'Nu';
+$_['text_enabled']            = 'Activat';
+$_['text_disabled']           = 'Dezactivat';
+$_['text_none']               = ' --- Nimic --- ';
+$_['text_select']             = ' --- Selectaţi --- ';
+$_['text_select_all']         = 'Selectare Toate';
+$_['text_unselect_all']       = 'Deselectare Toate';
+$_['text_all_zones']          = 'Toate Zonele';
+$_['text_default']            = ' <b>(Implicit)</b>';
+$_['text_close']              = 'Închidere';
+$_['text_pagination']         = 'Afişare {start} - {end} din {total} ({pages} Pagini)';
+$_['text_loading']            = 'Incărcare...';
+$_['text_no_results']         = 'Niciun rezultat!';
+$_['text_confirm']            = 'Sunteţi sigur?';
+$_['text_home']               = 'Acasă';
 
 // Button
 $_['button_add']                    = 'Add New';
